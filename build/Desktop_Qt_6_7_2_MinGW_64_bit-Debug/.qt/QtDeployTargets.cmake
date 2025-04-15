06395cdcd48071849d6e2d9cd04a4f5485c7f2fe
Furdow <117962858+JustSOOw@users.noreply.github.com>
2025-04-15 23:05:24 +0800

    Delete build directory

--- build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_IntelliMedia_Notes_FILE D:/Users/JUSTsoo/Desktop/IntelliMedia_Notes/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/IntelliMedia_Notes.exe)
-set(__QT_DEPLOY_TARGET_IntelliMedia_Notes_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_IntelliMedia_Notes_RUNTIME_DLLS E:/QT/6.7.2/mingw_64/bin/Qt6Widgets.dll;E:/QT/6.7.2/mingw_64/bin/Qt6Gui.dll;E:/QT/6.7.2/mingw_64/bin/Qt6Core.dll)
